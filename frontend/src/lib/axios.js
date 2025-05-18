8c8e464070126c6e02e44b98ab2e4daef52487e2
niteshmuchhala <137606385+niteshmuchhala@users.noreply.github.com>
2025-05-18 14:56:46 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-// use VITE_ env var (set in .env)
-const BASE_URL = import.meta.env.VITE_API_URL;
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://chatty-backend-47kr.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // needed if using cookies/auth headers
+  withCredentials: true,
 });
