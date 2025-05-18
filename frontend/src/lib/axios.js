29f288a9d5451822fb5752c56e387704c619c57c
niteshmuchhala <137606385+niteshmuchhala@users.noreply.github.com>
2025-05-18 13:53:54 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// Use absolute URL for production
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://chatty-backend-47kr.onrender.com/api";
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
